10baed6a513dc327f6d75404ecc2b97673d08e04
Your Name <you@example.com>
2023-03-21 09:30:02 +0100

    main

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,7 @@ export default function RootLayout({
     
     <html lang="en">
       <body>
+        
         <nav className='navigation'>
           <Link href='/'>Home</Link>
           <Link href='/blog'>Blog</Link>
